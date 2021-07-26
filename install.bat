cc7421d231a9e80322eb77e2554da48a8161f83b
ZeyyYourG <76854620+ZeyyYourG@users.noreply.github.com>
2021-07-26 16:05:49 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,15 @@
+
+
+@echo off
+
+cls
+color a
+title Zeyybre Installation
+echo Installing module
+call "./bin/1.bat
+call "./bin/2.bat
+call "./bin/3.bat
+call "./bin/4.bat
+call "./bin/5.bat
+echo Done installing modules
+PAUSE
